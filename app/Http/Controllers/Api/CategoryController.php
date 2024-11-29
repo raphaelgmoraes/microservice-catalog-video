e73fdf156d52509ff84c11ef455dc9f4189c5828
Raphael Moraes <raphael2k2moraes@gmail.com>
2024-11-28 22:03:58 -0300

    :sparkles: feat: Continue Implements TDD and Install Laravel 11

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use Core\UseCase\Category\CreateCategoryUseCase;
+use Core\UseCase\Category\DeleteCategoryUseCase;
+use Core\UseCase\Category\ListCategoriesUseCase;
+use Core\UseCase\Category\ListCategoryUseCase;
+use Core\UseCase\Category\UpdateCategoryUseCase;
+use Core\UseCase\DTO\Category\CategoryInputDto;
+use Core\UseCase\DTO\Category\CreateCategory\CategoryCreateInputDto;
+use Core\UseCase\DTO\Category\ListCategory\ListCategoriesInputDto;
+use Core\UseCase\DTO\Category\UpdateCategory\CategoryUpdateInputDto;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+
+class CategoryController extends Controller
+{
+    /**
+     * @param Request $request
+     * @param ListCategoriesUseCase $useCase
+     * @return AnonymousResourceCollection
+     */
+    public function index(Request $request, ListCategoriesUseCase $useCase): AnonymousResourceCollection
+    {
+        $response = $useCase->execute(
+            input: new ListCategoriesInputDto(
+                filter: $request->get('filter', ''),
+                sort: $request->get('sort', 'DESC'),
+                page: (int) $request->get('page', 1),
+                totalPage: (int) $request->get('total_page', 15),
+            )
+        );
+
+        return CategoryResource::collection(collect($response->items))
+                                    ->additional([
+                                        'meta' => [
+                                            'total' => $response->total,
+                                            'current_page' => $response->currentPage,
+                                            'last_page' => $response->lastPage,
+                                            'first_page' => $response->firstPage,
+                                            'per_page' => $response->perPage,
+                                            'to' => $response->to,
+                                            'from' => $response->from,
+                                        ],
+                                    ]);
+    }
+
+    /**
+     * @param StoreCategoryRequest $request
+     * @param CreateCategoryUseCase $useCase
+     * @return JsonResponse
+     */
+    public function store(StoreCategoryRequest $request, CreateCategoryUseCase $useCase): JsonResponse
+    {
+        $response = $useCase->execute(
+            input: new CategoryCreateInputDto(
+                name: $request->name,
+                description: $request->description ?? '',
+                active: (bool) $request->is_active ?? true,
+            )
+        );
+
+        return (new CategoryResource($response))
+                    ->response()
+                    ->setStatusCode(Response::HTTP_CREATED);
+    }
+
+    public function show(ListCategoryUseCase $useCase, $id)
+    {
+        $category = $useCase->execute(new CategoryInputDto($id));
+
+        return (new CategoryResource($category))->response();
+    }
+
+    public function update(UpdateCategoryRequest $request, UpdateCategoryUseCase $useCase, $id)
+    {
+        $response = $useCase->execute(
+            input: new CategoryUpdateInputDto(
+                id: $id,
+                name: $request->name,
+            )
+        );
+
+        return (new CategoryResource($response))->response();
+    }
+
+    public function destroy(DeleteCategoryUseCase $useCase, $id)
+    {
+        $useCase->execute(new CategoryInputDto($id));
+
+        return response()->noContent();
+    }
+}
---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'is_active' => $this->active,
+            'created_at' => now()->parse($this->created_at)
+                ->format('Y-m-d H:i:s'),
+        ];
+    }
+}
